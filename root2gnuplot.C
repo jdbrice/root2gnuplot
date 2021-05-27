1f4dd1008336a3de15f65a97ceca179d120d82d6
Daniel <jdb12@rice.edu>
2021-05-27 10:07:05 -0400

    Use single format function for consistency and code clarity

--- root2gnuplot.C
+++ root2gnuplot.C
@@ -21,9 +21,29 @@ using namespace std;
 
 const int col_width = 15;
 const int col_precision = 5;
+const std::string format_default_TH1 = "x y xlow xhigh ylow yhigh";
+const std::string format_default_TH2 = "x y z";
+const std::string format_default_TGraphErrors = "x y xlow xhigh ylow yhigh";
 
 class DataFileMaker {
     public:
+
+        struct PackedValues{
+            double x, y, z;
+            double xl, yl, zl;
+            double xh, yh, zh;
+
+            double dx(){
+                return xh - x;
+            }
+            double dy(){
+                return yh - y;
+            }
+            double dz(){
+                return zh - z;
+            }
+
+        };
 
         DataFileMaker() {}
         ~DataFileMaker() {}
@@ -44,8 +64,46 @@ class DataFileMaker {
             tmp << std::endl;
         }
 
-        // master convert (handles all histogram obects)
-        string convert( TH1 * h, string format, string fn, bool binary = false ) {
+
+        void format_line( std::ofstream &tmp, PackedValues &pv, string &format ){
+            // NOW parse the format string and push data out in that order
+            stringstream sstr( format );
+            string token = "";
+            string sep = "";
+            while( std::getline(sstr, token, ' ') ){
+
+                tmp << " " << sep << std::left << std::setw( col_width - 2 );
+                if ( "x" == token )
+                    tmp << pv.x;
+                if ( "xlow" == token )
+                    tmp << pv.xl;
+                if ( "xhigh" == token )
+                    tmp << pv.xh;
+                if ( "y" == token )
+                    tmp << pv.y;
+                if ( "ylow" == token )
+                    tmp << pv.yl;
+                if ( "yhigh" == token )
+                    tmp << pv.yh;
+                if ( "z" == token )
+                    tmp << pv.z;
+                if ( "zlow" == token )
+                    tmp << pv.zl;
+                if ( "zhigh" == token )
+                    tmp << pv.zh;
+                if ( "dx" == token )
+                    tmp << pv.dx();
+                if ( "dy" == token )
+                    tmp << pv.dy();
+                if ( "dz" == token )
+                    tmp << pv.dz();
+
+                sep = " ";
+            }
+        }
+
+        // master convert for all histogram obects (TH1, TH2, TH3)
+        string convert( TH1 * h, string format, string fn) {
 
             std::ofstream tmp;
             
@@ -73,8 +131,8 @@ class DataFileMaker {
             return fn; // return the filename
         }
 
-        // master convert (handles all TGraph obects)
-        string convert( TGraph * g, string format, string fn, bool binary = false ) {
+        // master convert for all TGraph obects
+        string convert( TGraph * g, string format, string fn ) {
             std::ofstream tmp;
             tmp.open (fn, std::ofstream::out );
             
@@ -87,206 +145,104 @@ class DataFileMaker {
         }
 
 
-        void convert_TH1( std::ofstream &tmp, TH1*h, string format, bool binary = false ){
+        void convert_TH1( std::ofstream &tmp, TH1*h, string format ){
             cout << "Converting TH1" << endl;
             if ( "" == format ){
-                format = "x y xlow xhigh ylow yhigh";
+                format = format_default_TH1;//"x y xlow xhigh ylow yhigh";
                 cout << TString::Format("No format provided, default is: %s", format.c_str() ) << endl;
             }
 
+            // Print the format to the file as column headers
             print_format( tmp, format );
 
-            vector<double> x, y, x_low, x_high, y_low, y_high;
+            PackedValues pv;
             for ( int i = 1; i < h->GetNbinsX() + 1; i++ ){
                 double vx = h->GetBinCenter( i );
                 double vy = h->GetBinContent( i );
                 double vey = h->GetBinError( i );
 
-                x.push_back( vx );
-                x_low.push_back( h->GetBinLowEdge(i) );
-                x_high.push_back( h->GetBinLowEdge(i) + h->GetBinWidth(i) );
-                y.push_back( vy );
-                y_low.push_back( vy - vey );
-                y_high.push_back( vy + vey );
-            }
+                pv.x = h->GetBinCenter( i );
+                pv.xl = h->GetBinLowEdge(i);
+                pv.xh = h->GetBinLowEdge(i) + h->GetBinWidth(i);
+                pv.y = h->GetBinContent( i );
+                pv.yl = pv.y - h->GetBinError( i );
+                pv.yh = pv.y + h->GetBinError( i );
 
-            assert( y.size() == x.size() );
-            assert( x_low.size() == x.size() );
-            assert( x_high.size() == x.size() );
-            assert( y_low.size() == x.size() );
-            assert( y_high.size() == x.size() );
-
-
-            for (unsigned int i = 0; i < x.size(); i++){
-
-                // NOW parse the format string and push data out in that order
-                stringstream sstr( format );
-                string token = "";
-                string sep = "";
-                while( std::getline(sstr, token, ' ') ){
-
-
-                    tmp << " " << sep << std::left << std::setw( col_width - 2 );
-                    if ( "x" == token )
-                        tmp << x[i];
-                    if ( "xlow" == token )
-                        tmp << x_low[i];
-                    if ( "xhigh" == token )
-                        tmp << x_high[i];
-                    if ( "y" == token )
-                        tmp << y[i];
-                    if ( "ylow" == token )
-                        tmp << y_low[i];
-                    if ( "yhigh" == token )
-                        tmp << y_high[i];
-                    if ( "dx" == token )
-                        tmp << (x_high[i] - x[i]);
-                    if ( "dy" == token )
-                        tmp << (y_high[i] - y[i]);
-
-                    sep = " ";
-                }
+                format_line( tmp, pv, format );
 
                 tmp << std::endl;
             }
 
         } // convert_TH1
 
-        void convert_TH2( std::ofstream &tmp, TH2*h, string format, bool binary = false ){
+        void convert_TH2( std::ofstream &tmp, TH2*h, string format ){
             cout << "Converting TH2" << endl;
 
             if ( "" == format ){
-                format = "x y z";
+                format = format_default_TH2;
                 cout << TString::Format("No format provided, default is: %s", format.c_str() ) << endl;
             }
 
+            // Print the format to the file as column headers
             print_format( tmp, format ) ;
 
-            
+            PackedValues pv;
             for ( int ix = 1; ix < h->GetNbinsX(); ix++ ){
                 for ( int iy = 1; iy < h->GetNbinsY(); iy++ ){
-                    
-                    double vx = h->GetXaxis()->GetBinCenter( ix );
-                    double vy = h->GetYaxis()->GetBinCenter( iy );
-                    double vz = h->GetBinContent( ix, iy );
-                    double vez = h->GetBinError( ix, iy );
-                    double vxlow = h->GetXaxis()->GetBinLowEdge(ix);
-                    double vxhigh = vxlow + h->GetXaxis()->GetBinWidth(ix);
 
-                    double vylow = h->GetYaxis()->GetBinLowEdge(iy);
-                    double vyhigh = vylow + h->GetYaxis()->GetBinWidth(iy);
+                    // pack the TH2 data into struct and then print formatted
+                    pv.x = h->GetXaxis()->GetBinCenter( ix );
+                    pv.y = h->GetYaxis()->GetBinCenter( iy );
+                    pv.z = h->GetBinContent( ix, iy );
+                    pv.zh = pv.z + h->GetBinError( ix, iy );
+                    pv.zl = pv.z - h->GetBinError( ix, iy );
+                    pv.xl = h->GetXaxis()->GetBinLowEdge(ix);
+                    pv.xh = pv.xl + h->GetXaxis()->GetBinWidth(ix);
+                    pv.yl = h->GetYaxis()->GetBinLowEdge(iy);
+                    pv.yh = pv.yl + h->GetYaxis()->GetBinWidth(iy);
 
-                    // NOW parse the format string and push data out in that order
-                    stringstream sstr( format );
-                    string token = "";
-                    string sep = "";
-                    while( std::getline(sstr, token, ' ') ){
-
-                        tmp << " " << sep << std::left << std::setw( col_width - 2 );
-                        if ( "x" == token )
-                            tmp << vx;
-                        if ( "xlow" == token )
-                            tmp << vxlow;
-                        if ( "xhigh" == token )
-                            tmp << vxhigh;
-                        if ( "y" == token )
-                            tmp << vy;
-                        if ( "ylow" == token )
-                            tmp << vylow;
-                        if ( "yhigh" == token )
-                            tmp << vyhigh;
-                        if ( "z" == token )
-                            tmp << vz;
-                        if ( "zlow" == token )
-                            tmp << vz-vez;
-                        if ( "zhigh" == token )
-                            tmp << vz+vez;
-                        if ( "dx" == token )
-                            tmp << (vxhigh - vx);
-                        if ( "dy" == token )
-                            tmp << (vyhigh - vy);
-                        if ( "dz" == token )
-                            tmp << (vez);
-
-                        sep = " ";
-                    }
-
+                    format_line( tmp, pv, format );
                     tmp << std::endl;
-                }
+                } // loop on iy
+                // double blank line per gnuplot data format
                 tmp << std::endl;
-            }
-
-            return;
-
+            } // loop on ix
         } // convert_TH2
 
-        void convert_TH3( std::ofstream &tmp, TH3*h, string format, bool binary = false ){
+        void convert_TH3( std::ofstream &tmp, TH3*h, string format ){
+            //TODO
+            cout << "TH3 is not implemented yet!" << endl;
         }
 
 
-        void convert_TGraphErrors( std::ofstream &tmp, TGraph*gr, string format, bool binary = false ){
+        void convert_TGraphErrors( std::ofstream &tmp, TGraph*gr, string format ){
             cout << "Converting a TGraphErrors" << endl;
 
             if ( "" == format ){
-                format = "x y xlow xhigh ylow yhigh";
+                format = format_default_TGraphErrors;
                 cout << TString::Format("No format provided, default is: %s", format.c_str() ) << endl;
             }
 
+            // Print the format to the file as column headers
             print_format( tmp, format );
 
-            vector<double> x, y, x_low, x_high, y_low, y_high;
+            PackedValues pv;
             for ( int i = 0; i < gr->GetN(); i++ ){
                 double vx = gr->GetX()[i];
                 double vy = gr->GetY()[i];
 
-                x.push_back( vx );
-                x_low.push_back( vx - gr->GetErrorXlow( i ) );
-                x_high.push_back( vx + gr->GetErrorXhigh( i ) );
-                y.push_back( vy );
-                y_low.push_back( vy - gr->GetErrorYlow( i ) );
-                y_high.push_back( vy + gr->GetErrorYhigh( i ) );
+                pv.x = gr->GetX()[i];
+                pv.xl = pv.x - gr->GetErrorXlow( i );
+                pv.xh = pv.x + gr->GetErrorXhigh( i );
+                pv.y = gr->GetY()[i];
+                pv.yl = pv.y - gr->GetErrorYlow( i );
+                pv.yh = pv.y + gr->GetErrorYhigh( i );
+
+                // print the formatted line
+                format_line( tmp, pv, format );
             }
 
-            assert( y.size() == x.size() );
-            assert( x_low.size() == x.size() );
-            assert( x_high.size() == x.size() );
-            assert( y_low.size() == x.size() );
-            assert( y_high.size() == x.size() );
-
-
-            for (unsigned int i = 0; i < x.size(); i++){
-
-                // NOW parse the format string and push data out in that order
-                stringstream sstr( format );
-                string token = "";
-                string sep = "";
-                while( std::getline(sstr, token, ' ') ){
-
-                    tmp << " " << sep << std::left << std::setw( col_width - 2 );
-                    if ( "x" == token )
-                        tmp << x[i];
-                    if ( "xlow" == token )
-                        tmp << x_low[i];
-                    if ( "xhigh" == token )
-                        tmp << x_high[i];
-                    if ( "y" == token )
-                        tmp << y[i];
-                    if ( "ylow" == token )
-                        tmp << y_low[i];
-                    if ( "yhigh" == token )
-                        tmp << y_high[i];
-                    if ( "dx" == token )
-                        tmp << (x_high[i] - x[i]);
-                    if ( "dy" == token )
-                        tmp << (y_high[i] - y[i]);
-
-                    sep = " ";
-                }
-
-                tmp << std::endl;
-            }
-
-        } // convert_TH1
+        } // convert_TGraph
 
 };
 
@@ -298,7 +254,22 @@ size_t verbosity = 1;
 void root2gnuplot( TString rootfile_hist = "", TString output = "out.dat", TString format = "" ){
 
     cout << "Usage:" << endl;
-    cout << "\tr2g input.root<:name> output.dat <format>, if <:name> omitted, convert all" << endl;
+    cout << "\troot2gnuplot.C <--help> input.root<:name> output.dat <format>, if <:name> omitted, convert all" << endl;
+    cout << "\t--help print more information" << endl;
+
+    if ( rootfile_hist == "--help" ){
+        cout << "help" << endl << endl;
+        cout << "format specifiers (case sensitive):" << endl;
+        cout << "\tx, y, z: coordinate from first, second, third axis for TH1, TH2, TH3 histograms" << endl << endl;
+        cout << "\t<axis>low: value on <axis> at lower error bar, example: xlow, ylow, zlow" << endl << endl;
+        cout << "\t<axis>high: value on <axis> at upper error bar, example: xhigh, yhigh, zhigh" << endl << endl;
+        cout << "\td<axis>: value on <axis> at upper error bar minus value on <axis>, example: dx, dy, dz" << endl << endl << endl;
+
+        cout << "default formats:" << endl;
+        cout << "\tTH1: " << format_default_TH1 << endl;
+        return;
+    }
+
 
     bool convert_all = rootfile_hist.First( ':' ) == -1;
     int index_split = rootfile_hist.First( ':' );
